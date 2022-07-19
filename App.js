d99b5df62f9d8ed40b81ae51bd89a2445afede25
beasxa <ambeasley98@gmail.com>
2022-07-19 13:56:03 -0700

    changed app.js

--- App.js
+++ App.js
@@ -1,37 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState, useCallback, useEffect} from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat';
-import db from "./firebase";
-import { collection, getDocs, doc, updateDoc, setDoc, arrayUnion, onSnapshot} from 'firebase/firestore';
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import ChatScreen from "./screens/ChatScreen";
-import HomeScreen from "./screens/HomeScreen";
+import React from 'react';
+import './firebase';
+import RootNavigation from './navigation/Index';
 
-const Stack = createStackNavigator();
-
-function App() {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Chat" component={ChatScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <RootNavigation />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
-});
-
-export default App;
