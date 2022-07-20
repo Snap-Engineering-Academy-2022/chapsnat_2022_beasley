ac0bb81d8850bd1bc73d1c5ea96247fe31a9f6e6
beasxa <ambeasley98@gmail.com>
2022-07-20 10:46:31 -0700

    working appgit add .git add . at the end of authentication lab

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,72 +1,100 @@
+import React, { useState, useEffect } from "react";
+import { Text, TextInput, View, TouchableOpacity, StyleSheet } from "react-native";
+import { getAuth, signOut, signInWithEmailAndPassword} from "firebase/auth";
+
 export default function LoginScreen({navigation}) {
-	const [email, setEmail] = useState();
-	const [password, setPassword] = useState();
-
-	return (
-		<>
-			<Text style={styles.bigBlue}>Login Here</Text>
-			<View style={styles.inputView}>
-				<TextInput
-					placeholder='Email'
-					placeholderTextColor="#003f5c"
-					onChangeText={(email) => setEmail(email)}
-				/>
-			</View>
-			<View style={styles.inputView}>
-				<TextInput
-					placeholder='Password'
-					secureTextEntry={true}
-					placeholderTextColor="#003f5c"
-					onChangeText={(password) => setPassword(password)}
-				/>
-			</View>
-			<TouchableOpacity style={styles.loginBtn} onPress={() => {
-				{/* what do you think will go here? */}
-			}}>
-				<Text style={styles.loginText}>LOGIN</Text>
-			</TouchableOpacity>
-		</>
-	)
+    //State Variables for user's email and password
+    const [email, setEmail] = useState();
+    const [password, setPassword] = useState();
+    //getAuth() actually authenticates the user, I think
+    const auth = getAuth();
+    //Very similar handleSubmit() function as signupScreen.js
+    //Instead, we have signIn with email/pass instead of creting new user
+    async function handleSubmit() {
+        console.log("handle submit envoked!!")
+        await signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            const user = userCredential.user;
+        })
+        //Not using error code again
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, "<---- error code");
+            console.log(errorMessage, "<--- error message")
+        });
+    }
+    console.log(auth, "<-----auth object")
+    //return outputs a screen for login where user can input email/pass
+    return (
+        <>
+            <Text style={styles.bigBlue}>Login Here</Text>
+            <View style={styles.inputView}>
+                <TextInput
+                    placeholder='Email'
+                    placeholderTextColor="#003F5C"
+                    onChangeText={(email) => setEmail(email)}
+                />
+            </View>
+            <View style={styles.inputView}>
+                <TextInput
+                    placeholder='Password'
+                    secureTextEntry={true}
+                    placeholderTextColor="#003F5C"
+                    onChangeText={(password) => setPassword(password)}
+                />
+            </View>
+            <TouchableOpacity style={styles.loginBtn} onPress={() => {
+                handleSubmit();
+            }}>
+                <Text style={styles.loginText}>LOGIN</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.redirectBtn} onPress={() => {
+                navigation.navigate("Signup")
+            }}>
+                <Text>Don't have an account? Sign up here</Text>
+            </TouchableOpacity>
+        </>
+    )
 }
-
 const styles = StyleSheet.create({
-	redirectBtn: {
-		width:"80%",
-		borderRadius:25,
-		height:50,
-		alignItems:"center",
-		justifyContent:"center",
-		marginTop:40,
-		backgroundColor:"grey",
-		color: "white"
-	},
-	inputView: {
-		backgroundColor: "#FFC0CB",
-		borderRadius: 30,
-		width: "70%",
-		height: 45,
-		marginBottom: 20,
-		alignItems: "center",
-	},
-	TextInput: {
-		height: 50,
-		flex: 1,
-		padding: 10,
-		marginLeft: 20,
-	},
-	loginBtn: {
-		width:"80%",
-		borderRadius:25,
-		height:50,
-		alignItems:"center",
-		justifyContent:"center",
-		marginTop:40,
-		backgroundColor:"#FF1493",
-	},
-	bigBlue: {
-		color: 'blue',
-		fontWeight: 'bold',
-		fontSize: 30,
-		padding: 50
-	}
+    redirectBtn: {
+        width:"80%",
+        borderRadius:25,
+        height:50,
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop:40,
+        backgroundColor:"grey",
+        color: "white"
+    },
+    inputView: {
+        backgroundColor: "#FFC0CB",
+        borderRadius: 30,
+        width: "70%",
+        height: 45,
+        marginBottom: 20,
+        alignItems: "center",
+    },
+    TextInput: {
+        height: 50,
+        flex: 1,
+        padding: 10,
+        marginLeft: 20,
+    },
+    loginBtn: {
+        width:"80%",
+        borderRadius:25,
+        height:50,
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop:40,
+        backgroundColor:"#FF1493",
+    },
+    bigBlue: {
+        color: 'blue',
+        fontWeight: 'bold',
+        fontSize: 30,
+        padding: 50
+    }
 })
